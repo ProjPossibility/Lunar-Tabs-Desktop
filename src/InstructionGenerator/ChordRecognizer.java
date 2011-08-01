fcc3322e8ebc48182a4ff4ffb89dc565bd21c01b
prateek1.tandon@gmail.com <prateek1.tandon@gmail.com@d034e982-556d-11de-833c-a7bd19eed978>
2011-08-01 19:41:54 +0000

    Infrastructure

---
+++ src/InstructionGenerator/ChordRecognizer.java
@@ -0,0 +1,14 @@
+package InstructionGenerator;
+import java.util.*;
+import org.herac.tuxguitar.song.models.TGBeat;
+import org.herac.tuxguitar.song.models.TGNote;
+import org.herac.tuxguitar.song.models.TGVoice;
+import org.herac.tuxguitar.song.models.TGDuration;
+
+public class ChordRecognizer {
+	
+	public static String getChordName(List<TGNote> notes) {
+		return "A minor";
+	}
+	
+}
